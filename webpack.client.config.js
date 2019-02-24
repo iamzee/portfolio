5b9c0f7195282b6afd093b5b0e23fa47652bcf41
iamzee <zeeshan99adeen@gmail.com>
2019-02-25 00:11:28 +0530

    webpack config for production

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,22 +1,45 @@
 const path = require ('path');
+const ExtractTextPlgin = require ('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './client/index.js',
-  output: {
-    path: path.resolve (__dirname, 'public'),
-    filename: 'client.bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
+module.exports = env => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlgin ('styles.css');
+
+  return {
+    entry: './client/index.js',
+    output: {
+      path: path.resolve (__dirname, 'public'),
+      filename: 'client.bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract ({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [CSSExtract],
+  };
 };
